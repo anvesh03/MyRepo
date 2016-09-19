d3f211011a4f2815eba50666f81f360929d4a115
anvesh03 <a.anvesh03@gmail.com>
2016-09-19 17:40:14 +0530

    changes from local to RemoteBranch

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ public static void Main(String[] args)
 System.out.println("welcome");
 system.out.println("welocome:Remote Repostiory");
 System.out.println("welcome to :Local Branch");
+system.out.println("from local dev changes to remote")
 }
 }
