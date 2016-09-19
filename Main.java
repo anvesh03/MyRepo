65a058b6ff1d05543c622e518a72b9ab78e879d4
anvesh03 <a.anvesh03@gmail.com>
2016-09-19 17:31:19 +0530

    committed to local branch.

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ class Main{
 public static void Main(String[] args)
 System.out.println("welcome");
 system.out.println("welocome:Remote Repostiory");
+System.out.println("welcome to :Local Branch");
 }
 }
